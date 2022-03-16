90f0216e89d43d057e589f83729b5e0eb5797513
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-03-15 17:02:35 -0700

    add words

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -10,28 +10,61 @@ import "hardhat/console.sol";
 // We inherit the contract we imported. This means we'll have access
 // to the inherited contract's methods.
 contract MyEpicNFT is ERC721URIStorage {
-  // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
+    // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    string[] firstWords = [
+        "coat",
+        "throat",
+        "boat",
+        "goat",
+        "cloak",
+        "vote"
+    ];
 
-  // We need to pass the name of our NFTs token and its symbol.
-  constructor() ERC721 ("SquareNFT", "SQUARE") {
-    console.log("This is my NFT contract. Woah!");
-  }
+    string[] secondWords = [
+        "begin",
+        "thin",
+        "sin",
+        "win",
+        "ally",
+        "sucks"
+    ];
 
-  // A function our user will hit to get their NFT.
-  function makeAnEpicNFT() public {
-     // Get the current tokenId, this starts at 0.
-    uint256 newItemId = _tokenIds.current();
+    string[] thirdWords = [
+        "something",
+        "strange",
+        "went",
+        "bathromm",
+        "poop",
+        "scoop"
+    ];
 
-     // Actually mint the NFT to the sender using msg.sender.
-    _safeMint(msg.sender, newItemId);
+    // We need to pass the name of our NFTs token and its symbol.
+    constructor() ERC721("SquareNFT", "SQUARE") {
+        console.log("This is my NFT contract. Woah!");
+    }
 
-    // Set the NFTs data.
-    _setTokenURI(newItemId, "data:application/json;base64,eyJuYW1lIjoiUG9ja2V0IENlcmVhbCIsImRlc2NyaXB0aW9uIjoic3F1aXJyZWwgYW5kIG51dHMiLCJpbWFnZSI6ImRhdGE6aW1hZ2Uvc3ZnK3htbDtiYXNlNjQsUEhOMlp5QjRiV3h1Y3owaWFIUjBjRG92TDNkM2R5NTNNeTV2Y21jdk1qQXdNQzl6ZG1jaUlIQnlaWE5sY25abFFYTndaV04wVW1GMGFXODlJbmhOYVc1WlRXbHVJRzFsWlhRaUlIWnBaWGRDYjNnOUlqQWdNQ0F6TlRBZ016VXdJajRLSUNBZ0lEeHpkSGxzWlQ0dVltRnpaU0I3SUdacGJHdzZJSGRvYVhSbE95Qm1iMjUwTFdaaGJXbHNlVG9nYzJWeWFXWTdJR1p2Ym5RdGMybDZaVG9nTVRSd2VEc2dmVHd2YzNSNWJHVStDaUFnSUNBOGNtVmpkQ0IzYVdSMGFEMGlNVEF3SlNJZ2FHVnBaMmgwUFNJeE1EQWxJaUJtYVd4c1BTSmliR0ZqYXlJZ0x6NEtJQ0FnSUR4MFpYaDBJSGc5SWpVd0pTSWdlVDBpTlRBbElpQmpiR0Z6Y3owaVltRnpaU0lnWkc5dGFXNWhiblF0WW1GelpXeHBibVU5SW0xcFpHUnNaU0lnZEdWNGRDMWhibU5vYjNJOUltMXBaR1JzWlNJK2NHOWphMlYwWTJWeVpXRnNQQzkwWlhoMFBnbzhMM04yWno0PSJ9");
-    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+    // A function our user will hit to get their NFT.
+    function makeAnEpicNFT() public {
+        // Get the current tokenId, this starts at 0.
+        uint256 newItemId = _tokenIds.current();
 
-    // Increment the counter for when the next NFT is minted.
-    _tokenIds.increment();
-  }
+        // Actually mint the NFT to the sender using msg.sender.
+        _safeMint(msg.sender, newItemId);
+
+        // Set the NFTs data.
+        _setTokenURI(
+            newItemId,
+            "data:application/json;base64,eyJuYW1lIjoiUG9ja2V0IENlcmVhbCIsImRlc2NyaXB0aW9uIjoic3F1aXJyZWwgYW5kIG51dHMiLCJpbWFnZSI6ImRhdGE6aW1hZ2Uvc3ZnK3htbDtiYXNlNjQsUEhOMlp5QjRiV3h1Y3owaWFIUjBjRG92TDNkM2R5NTNNeTV2Y21jdk1qQXdNQzl6ZG1jaUlIQnlaWE5sY25abFFYTndaV04wVW1GMGFXODlJbmhOYVc1WlRXbHVJRzFsWlhRaUlIWnBaWGRDYjNnOUlqQWdNQ0F6TlRBZ016VXdJajRLSUNBZ0lEeHpkSGxzWlQ0dVltRnpaU0I3SUdacGJHdzZJSGRvYVhSbE95Qm1iMjUwTFdaaGJXbHNlVG9nYzJWeWFXWTdJR1p2Ym5RdGMybDZaVG9nTVRSd2VEc2dmVHd2YzNSNWJHVStDaUFnSUNBOGNtVmpkQ0IzYVdSMGFEMGlNVEF3SlNJZ2FHVnBaMmgwUFNJeE1EQWxJaUJtYVd4c1BTSmliR0ZqYXlJZ0x6NEtJQ0FnSUR4MFpYaDBJSGc5SWpVd0pTSWdlVDBpTlRBbElpQmpiR0Z6Y3owaVltRnpaU0lnWkc5dGFXNWhiblF0WW1GelpXeHBibVU5SW0xcFpHUnNaU0lnZEdWNGRDMWhibU5vYjNJOUltMXBaR1JzWlNJK2NHOWphMlYwWTJWeVpXRnNQQzkwWlhoMFBnbzhMM04yWno0PSJ9"
+        );
+        console.log(
+            "An NFT w/ ID %s has been minted to %s",
+            newItemId,
+            msg.sender
+        );
+
+        // Increment the counter for when the next NFT is minted.
+        _tokenIds.increment();
+    }
 }
